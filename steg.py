03e80df5f466e667aed878863fd33fd1ee04de60
Priyanka H. Khare <62902884+ScotchGeek@users.noreply.github.com>
2020-09-06 21:15:45 +0530

    Update steg.py

--- steg.py
+++ steg.py
@@ -91,7 +91,7 @@ def encode():
 	encode_enc(newimg, data)
 
 	new_img_name = input("Enter the name of new image(with extension) : ")
-	newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
+	newimg.save(new_img_name )
 
 # Decode the data in the image
 def decode():
